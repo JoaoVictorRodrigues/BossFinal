b77e0b3b020b0189f17a76273c4caa41f59cdd31
JoaoVictorRodrigues <jvictor.rodrigues08@gmail.com>
2021-05-14 16:41:03 -0300

    Comeco da mecanica

---
+++ Assets/_Scripts/Obstacle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public float speed;
+    public float screenEnd;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.down *speed*Time.deltaTime);
+        if (transform.position.y < screenEnd){
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/_Scripts/PlayerL.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerL : MonoBehaviour
+{
+    private Vector2 pos;
+    public float IncX;
+    public float maxWidth;
+    public float minWidth;
+    public float speed;
+    
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, pos, speed*Time.deltaTime);
+        if(Input.GetKeyDown(KeyCode.RightArrow) && transform.position.x < maxWidth){
+            pos = new Vector2(transform.position.x + IncX, transform.position.y);
+            transform.position = pos;
+        }else if(Input.GetKeyDown(KeyCode.LeftArrow) && transform.position.x > minWidth){
+            pos = new Vector2(transform.position.x - IncX, transform.position.y);
+            transform.position = pos;
+        }
+    }
+    
+}
