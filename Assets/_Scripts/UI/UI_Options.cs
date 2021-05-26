520a05382be6b497581cc4497d3e72444c42c796
Evandro Romeiro <evandrofr@al.insper.edu.br>
2021-05-26 11:58:54 -0300

    Options + Audio

---
+++ Assets/_Scripts/UI/UI_Options.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Options : MonoBehaviour{
+    GameManager gm;
+
+    private void OnEnable(){
+        gm = GameManager.GetInstance();
+    }
+
+    public void Voltar(){
+        gm.ChangeState(GameManager.GameState.MENU);
+    }
+
+}
